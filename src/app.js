ce7ea66206576f32aa8532aaabda215814f3a852
Bassit owolabi <barseetbrown@gmail.com>
2022-07-05 08:15:54 +0100

    feat: landing page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -9,4 +10,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+const publicDirectory = path.join(__dirname, '../public')
+app.use(express.static(publicDirectory))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
 module.exports = app
